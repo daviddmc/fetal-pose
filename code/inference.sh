a9c0c93bf312d76aeec4c46924adff0c21c72105
daviddmc <junshenxu1996@gmail.com>
2022-12-02 13:36:29 +0000

    rm inference.sh

--- code/inference.sh
+++
@@ -1,4 +0,0 @@
-python inference.py --name=b8s64bn_rot_scale0.2_zoom_0.5_1.5_adamwr1_flip_unet_all_400ep_new \
-                    --gpu_id=6 \
-                    --rawdata_path=test_split \
-                    --output_label=test_joint.mat
